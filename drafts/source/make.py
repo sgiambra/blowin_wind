194522d23a76dda811d92e7cbe555c0adae7b642
Samuele Giambra <samuele_giambra@brown.edu>
2018-05-12 20:02:35 +0200

    First pass of draft

---
+++ drafts/source/make.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+#****************************************************
+# GET LIBRARY
+#****************************************************
+import subprocess, shutil, os
+from gslab_make.dir_mod import *
+from gslab_make.get_externals import *
+from gslab_make.make_log import *
+from gslab_make.make_links import *
+from gslab_make.make_link_logs import *
+from gslab_make.run_program import *
+
+#****************************************************
+# MAKE.PY STARTS
+#****************************************************
+set_option(link_logs_dir = '../output')
+clear_dirs('../output', '../temp')
+start_make_logging()
+
+from gslab_fill.tablefill import tablefill
+from gslab_fill.textfill import textfill
+
+tablefill(
+    input = '../../analysis/covariates_balance/output/tables.txt '
+            '../../analysis/descriptive_regressions/output/tables.txt',
+    template = './tables.lyx',
+    output = '../output/tables_filled.lyx'
+    )
+
+# COMPILE (ORDER MATTERS)
+run_lyx(program = './blowin_wind')
+
+end_make_logging()
+
+raw_input('\n Press <Enter> to exit.')
